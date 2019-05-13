08b1774bb8091f033394f26eb8ef684f67da2e6b
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-05-13 15:43:04 -0500

    guide finished

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -18,6 +19,12 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  // generate a hash of the user's password
+  // we'll do it synchronously
+
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -33,9 +40,9 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
+      } else{
         res.status(401).json({ message: 'Invalid Credentials' });
       }
     })
@@ -44,13 +51,35 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
+
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next) {
+  const {username, password} = req.headers;
+
+  if(username && password) {
+    Users.findBy( username )
+      .first()
+      .then(user =>{
+        if(user && bcrypt.compareSync(password, User.password)){
+          next();
+        } else {
+          res.status(401).json({message: 'Invalid Credentials'});
+        }
+      })
+      .catch(error =>{
+        res.status(500).json({message: " Unexpected error! "})
+      })
+  } else {
+    res.status(400).json({message: 'incomplete credentials'})
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
